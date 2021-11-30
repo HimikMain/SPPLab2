72dcb74a2ecf9663651862970ee3f0460e8eb152
Anatol <64561080+HimikMain@users.noreply.github.com>
2021-11-30 23:29:22 +0300

    2-ая лаба -_-

---
+++ SPPLab2/SPPLab2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+
+namespace SPPLab2
+{
+    public delegate void TaskDelegate(string file1, string file2);
+
+    static class Program
+    {
+        delegate void TaskDelegate();
+
+        static void Main(string[] args)
+        {
+            int countfFiles = 0;
+            Console.WriteLine("Введите исходный каталог");
+            String sourcePath = Console.ReadLine();
+            sourcePath = @"C:\Labs\!Tet\1";
+            Console.WriteLine("Введите целевой каталог");
+            String targetPath = Console.ReadLine();
+            targetPath = @"C:\Labs\!Tet\2";
+
+            TaskQueue task = new TaskQueue(3);
+
+            DirectoryCopy(sourcePath, targetPath, task, ref countfFiles);
+
+            task.CloseTasks();
+
+            Console.WriteLine("Файлов скопировано {0}", countfFiles);
+            Console.ReadKey();
+        }
+
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, TaskQueue task, ref int numberOfFiles)
+        {
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException("Каталог не найден: " + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                if (!File.Exists(temppath))
+                {
+                    task.EnqueueTask(delegate () { file.CopyTo(temppath, false); });
+                    numberOfFiles++;
+                }
+            }
+
+            foreach (DirectoryInfo subdir in dirs)
+            {
+                string temppath = Path.Combine(destDirName, subdir.Name);
+                DirectoryCopy(subdir.FullName, temppath, task, ref numberOfFiles);
+            }
+
+        }
+    }
+}
---
+++ SPPLab2/SPPLab2/TaskQueue.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Concurrent;
+
+namespace SPPLab2
+{
+    public class TaskQueue
+    {
+        bool working;
+
+        private Thread[] threads;
+        public delegate void TaskDelegate();
+        private TaskDelegate[] threadTasks;
+        private object[] threadLockers;
+
+        private ConcurrentQueue<TaskDelegate> queue;
+        private Thread dequeueThread;
+
+        public TaskQueue(int count)
+        {
+            if (count < 1)
+            {
+                throw new ArgumentOutOfRangeException("count", "Количество потоков должно быть больше нуля");
+            }
+
+            working = true;
+
+            threads = new Thread[count];
+            threadTasks = new TaskDelegate[count];
+            threadLockers = new object[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                threadLockers[i] = new object();
+                threads[i] = new Thread(() =>
+                {
+                    int index = Convert.ToInt32(Thread.CurrentThread.Name);
+                    while (working)
+                    {
+                        lock (threadLockers[index])
+                        {
+                            threadTasks[index]?.Invoke();
+                            threadTasks[index] = null;
+                        }
+
+                        Thread.Sleep(0);
+                    }
+                });
+                threads[i].Name = i.ToString();
+                threads[i].Start();
+            }
+
+            queue = new ConcurrentQueue<TaskDelegate>();
+            dequeueThread = new Thread(DequeueTasks);
+            dequeueThread.Start();
+        }
+
+        public void EnqueueTask(TaskDelegate task)
+        {
+            queue.Enqueue(task);
+        }
+
+        public void CloseTasks()
+        {
+            bool noTasks;
+            do
+            {
+                noTasks = queue.Count == 0;
+                for (int i = 0; i < threads.Length; i++)
+                {
+                    noTasks = noTasks && threadTasks[i] == null;
+                }
+            }
+            while (!noTasks);
+
+            working = false;
+        }
+
+        private void DequeueTasks()
+        {
+            while (working)
+            {
+                for (int i = 0; i < threads.Length; i++)
+                {
+                    if (threadTasks[i] == null)
+                    {
+                        lock (threadLockers[i]){
+                            while (!queue.TryDequeue(out threadTasks[i]) && working) { }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
